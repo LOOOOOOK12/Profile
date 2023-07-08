a2ecd1f478fb999f9bcfc4dedea426d84b053787
Luke Symon <lukealamil12@gmail.com>
2023-07-08 15:02:15 +0800

    added burgir function

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const nav = document.getElementById("nav-bar");
 const up = document.getElementById("Up");
 
 const li = document.querySelectorAll(".links");
-const sec = document.querySelectorAll("section")
+const sec = document.querySelectorAll("section");
+
+const burgir = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+burgir.addEventListener("click", () => {
+  burgir.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".links").forEach(n => n.addEventListener("click", ()=>{
+  burgir.classList.remove("active");
+  navMenu.classList.remove("active");
+
+}))
 
 // window.addEventListener('scroll', ()=>{
 //     if(window.scrollY >=  50){  
