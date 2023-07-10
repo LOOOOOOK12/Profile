3b86f0966a225844f68092ae5cdf7fe0248c6d32
Luke Symon <lukealamil12@gmail.com>
2023-07-10 16:00:45 +0800

    update

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ up.addEventListener('click', () => {
     });
   });
 
-function activeMenu(){
-    let len = sec.length;
-    while(--len && window.scrollY+ 97 < sec[len].offsetTop){}
-    li.forEach(ltx => ltx.classList.remove("active"));
-    li[len].classList.add("active");
-}
-activeMenu();
+// function activeMenu(){
+//     let len = sec.length;
+//     while(--len && window.scrollY+ 97 < sec[len].offsetTop){}
+//     li.forEach(ltx => ltx.classList.remove("active"));
+//     li[len].classList.add("active");
+// }
+// activeMenu();
 
-window.addEventListener("scroll",activeMenu);
+// window.addEventListener("scroll",activeMenu);
 
 
   
